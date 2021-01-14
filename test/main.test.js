d47b2001b5ab37e106df3b609d5a0b5ade45d25f
Jaime Escalona <jaescalo@akamai.com>
2021-01-14 15:19:39 -0600

    Response header with cache tags.

--- test/main.test.js
+++ test/main.test.js
@@ -1,6 +1,9 @@
-import { sum } from '../main.js';
 // Require the built in 'assertion' library
 import { strict as assert } from 'assert';
+
+function sum(a, b) {
+  return a + b;
+}
 
 describe('Akamai EdgeWorkers Sum', function() {
   it('Should sum 1 + 2 and expect 3', function() {  
