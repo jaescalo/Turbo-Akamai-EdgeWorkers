ad35272be2b0f33df043c39cecaac18b8b5f84d6
Jaime Escalona <jaescalo@akamai.com>
2021-06-08 14:48:27 -0600

    Test for CircleCI Dynamic Configs

---
+++ scripts/update-ew-config-files.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+set -o pipefail
+
+EWVERSION=$1 #Edgeworkers Version
+DESCRIPTION=$2 #Description
+USE_EDGEKV=$3 #Push to production? (true|false)
+ACTIVATE_PRODUCTION=$4 #Use EdgeKV? (true|false)
+
+# Update the bundle.json
+sed -i 's|EWVERSION|'$EWVERSION'|g' bundle.json
+sed -i "s|DESCRIPTION|$DESCRIPTION|g" bundle.json
+
+# Update the akamai-ew.yml
+sed -i 's|EWVERSION|'$EWVERSION'|g' ./configs/akamai-ew.yml
+sed -i 's|USE_EDGEKV|'$USE_EDGEKV'|g' ./configs/akamai-ew.yml
+sed -i 's|ACTIVATE_PRODUCTION|'$ACTIVATE_PRODUCTION'|g' ./configs/akamai-ew.yml
